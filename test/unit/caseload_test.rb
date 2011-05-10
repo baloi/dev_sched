15962e589105cac21add72b579b5fc6874ff57e2
baloi <baloi.galia@gmail.com>
2011-05-09 22:34:45 -0400

    starting to work on caseloads..

--- test/unit/caseload_test.rb
+++ test/unit/caseload_test.rb
@@ -10,6 +10,23 @@ class CaseloadTest < Test::Unit::TestCase
     assert true
   end
 
+  def test_associations
+    # each caseload should belong to a rehab day
+    # each rehab day should have one to many caseloads
+
+    # each caseload should have one therapist
+    # each therapist can have many caseloads but only one for the day
+
+    # each caseload should have sessions
+    # each session should belong to only one caseload
+
+    
+  end
+
+  def test_group_day
+
+  end
+
   def test_one_caseload
     
     rehab_day_id = 1
