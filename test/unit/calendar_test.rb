a9335815ab8b83e8231fc39e45030e0c25f99d64
baloi <baloi.galia@gmail.com>
2011-07-16 12:25:59 -0400

    calendar_test

--- test/unit/calendar_test.rb
+++ test/unit/calendar_test.rb
@@ -7,6 +7,24 @@ class CalendarTest < Test::Unit::TestCase
     min = 30
     t = Time.mktime(day.year, day.month, day.date, hour, min)
     assert_equal t, SimpleTime.eight_thirty_am(day)
+
+    day = Day.new(2011, 2, 1)
+    hour = 9
+    min = 0
+    t = Time.mktime(day.year, day.month, day.date, hour, min)
+    assert_equal t, SimpleTime.nine_o_clock_am(day)
+
+  end
+
+  def test_create_day
+    day = Day.new(2011, 2, 1)
+    day_time = Calendar.create_day_time(day, "9:00")
+    t = Time.mktime(day.year, day.month, day.date, 9)
+    assert_equal t, day_time
+  end
+
+private
+  def create_day
   end
 end
 
